521be41928a64a90e1f80005b1cabef762f78aa8
deadsmond <35631174+deadsmond@users.noreply.github.com>
2020-01-08 09:45:40 +0100

    Update s4.bat

--- s4.bat
+++ s4.bat
@@ -1,1 +1,26 @@
 
+
+
+%USERNAME% 
+https://stackoverflow.com/questions/2866117/windows-batch-script-to-read-an-ini-file
+@setlocal enableextensions enabledelayedexpansion
+@echo off
+set file=%~1
+set area=[%~2]
+set key=%~3
+set currarea=
+for /f "usebackq delims=" %%a in ("!file!") do (
+    set ln=%%a
+    if "x!ln:~0,1!"=="x[" (
+        set currarea=!ln!
+    ) else (
+        for /f "tokens=1,2 delims==" %%b in ("!ln!") do (
+            set currkey=%%b
+            set currval=%%c
+            if "x!area!"=="x!currarea!" if "x!key!"=="x!currkey!" (
+                echo !currval!
+            )
+        )
+    )
+)
+endlocal
